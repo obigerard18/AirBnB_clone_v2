0f701b7813c1177b5a28f9006c5205ae9ef7eac2
obigerard18 <obigerard18@gmail.com>
2023-03-22 08:00:25 -0700

    __init__ file

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Generate a unique storage instance"""
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
